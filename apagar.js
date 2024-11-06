aa84ef9605f84bc3dfdf24566fd6cfa8bedf7da7
maria <maria>
2024-11-06 11:04:25 -0300

    prova

---
+++ apagar.js
@@ -0,0 +1,26 @@
+
+function removerCard1() {
+    document.getElementById('card1').removeChild(card1);
+}
+
+document.getElementById('botao1').addEventListener('click', removeCard1)
+
+
+function removerCard2() {
+    document.getElementById('card2').removeChild(card2);
+}
+
+document.getElementById('botao2').addEventListener('click', removerCard2)
+
+function removerCard3() {
+    document.getElementById('card3').removeChild(card3);
+}
+
+document.getElementById('botao3').addEventListener('click', removerCard3)
+
+function removerCard4() {
+    document.getElementById('card4').removeChild(card4);
+}
+
+document.getElementById('botao4').addEventListener('click', removerCard4)
+
